7811458359ff2fb13185a24529778931c7033f83
Darren Batarina <batarina.darren@gmail.com>
2025-11-21 23:32:13 +1100

    CLI app to produce a reward 1/4 times, with the reward being 1 of 4 items

---
+++ src/main/java/IntermittentReinforcement.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IntermittentReinforcement {
+
+    public IntermittentReinforcement() {}
+
+    private static final String[] REWARDS = {"A", "B", "C", "D"};
+    private static final float REWARD_PROBABILITY = 0.25f;
+
+    public String getReward() {
+
+        if (Math.random() <=  REWARD_PROBABILITY) {
+            return "No reward.";
+        }
+
+        return REWARDS[(int) (Math.random() * REWARDS.length)];
+
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        IntermittentReinforcement intermittentReinforcement = new IntermittentReinforcement();
+
+        System.out.println(intermittentReinforcement.getReward());
+
+    }
+
+}
